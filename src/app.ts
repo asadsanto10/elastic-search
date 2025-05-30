91649692761a13426e1c28860084d50595384577
asadsanto10 <asad.santo.20@gmail.com>
2025-05-30 22:22:56 +0600

    initial upload

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application } from "express";
+import { esClient } from "./app/elastic/es-client";
 
 const port: number | string = process.env.PORT || 5000;
 
@@ -12,9 +13,18 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.json({ message: "all ok..." });
 });
+
+esClient
+  .ping()
+  .then(() => {
+    console.log("Elastic run...");
+  })
+  .catch((err: Error) => {
+    console.log("Elastic error::" + err);
+  });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
