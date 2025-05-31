7f9423320a457f2f1d68cbcb95472d09545b89ff
asadsanto10 <asad.santo.20@gmail.com>
2025-05-31 19:56:32 +0600

    apm

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import apm from "elastic-apm-node";
 import express, { Application } from "express";
 import { esClient } from "./app/elastic/es-client";
 
 const port: number | string = process.env.PORT || 5000;
+
+apm.start({
+  serviceName: "testService",
+  serverUrl: "http://localhost:8200",
+  environment: "development",
+  captureBody: "all",
+});
 
 const app: Application = express();
 // parser
@@ -15,6 +23,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   res.json({ message: "all ok..." });
+});
+
+app.get("/users", (req, res) => {
+  const trans = apm.startTransaction("GET /users route");
+
+  let count = 0;
+
+  for (let index = 0; index < 999999999; index++) {
+    count += 1;
+  }
+
+  res.json({ count });
+  trans.end();
 });
 
 esClient
